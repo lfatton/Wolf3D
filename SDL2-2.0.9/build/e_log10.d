2a2f87f0e48bf5fe0f33aea55829a46ca1cfcbc8
MattDirty <mtorsell@student.42.fr>
2019-01-15 22:23:06 +0100

    trying to add sdl2

---
+++ SDL2-2.0.9/build/e_log10.d
@@ -0,0 +1,10 @@
+build/e_log10.lo: \
+  /Users/mtorsell/CLionProjects/Wolf3D/SDL2-2.0.9/src/libm/e_log10.c \
+  /Users/mtorsell/CLionProjects/Wolf3D/SDL2-2.0.9/src/libm/math_libm.h \
+  /Users/mtorsell/CLionProjects/Wolf3D/SDL2-2.0.9/src/libm/../SDL_internal.h \
+  /Users/mtorsell/CLionProjects/Wolf3D/SDL2-2.0.9/src/libm/../dynapi/SDL_dynapi.h \
+  /Users/mtorsell/CLionProjects/Wolf3D/SDL2-2.0.9/src/libm/../dynapi/SDL_dynapi_overrides.h \
+  include/SDL_config.h include/SDL_platform.h include/begin_code.h \
+  include/close_code.h \
+  /Users/mtorsell/CLionProjects/Wolf3D/SDL2-2.0.9/src/libm/math_private.h \
+  include/SDL_endian.h include/SDL_stdinc.h
