1da88c228e13db6126a9efe777f2a61431c4f949
MattDirty <mtorsell@student.42.fr>
2019-01-19 00:54:28 +0100

    added a test map and patched some issues that were breaking the soft.

--- srcs/map_verifs.c
+++ srcs/map_verifs.c
@@ -59,8 +59,8 @@ void				verify_map(enum e_tile **til)
 	(void)apply_to_whole_map(til, &put_wall_if_border);
 	if (apply_to_whole_map(til, &is_not_p_spawn))
 	{
-		x = rand() % (MAP_W - 3) + 1;
-		y = rand() % (MAP_H - 3) + 1;
+		x = rand() % (MAP_W - 3) + 2;
+		y = rand() % (MAP_H - 3) + 2;
 		til[y][x] = t_p_spawn;
 		til[y + 1][x] = t_nil;
 		til[y - 1][x] = t_nil;
