a35400dc9c4bc949a504331c342a5d4580080aad
MattDirty <mtorsell@student.42.fr>
2019-01-18 05:29:25 +0100

    normed changes

--- srcs/map_verifs.c
+++ srcs/map_verifs.c
@@ -53,22 +53,22 @@ int					apply_to_whole_map(enum e_tile **til,
 
 void				verify_map(enum e_tile **til)
 {
-	Uint32				x;
-	Uint32				y;
-	(void)apply_to_whole_map(til, &put_wall_if_border);
+	Uint32		x;
+	Uint32		y;
 
+	(void)apply_to_whole_map(til, &put_wall_if_border);
 	if (apply_to_whole_map(til, &is_not_p_spawn))
 	{
 		x = rand() % (MAP_W - 3) + 1;
 		y = rand() % (MAP_H - 3) + 1;
 		til[y][x] = t_p_spawn;
-		til[y+1][x] = t_nil;
-		til[y-1][x] = t_nil;
-		til[y][x+1] = t_nil;
-		til[y][x-1] = t_nil;
-		til[y+1][x+1] = t_nil;
-		til[y+1][x-1] = t_nil;
-		til[y-1][x-1] = t_nil;
-		til[y-1][x+1] = t_nil;
+		til[y + 1][x] = t_nil;
+		til[y - 1][x] = t_nil;
+		til[y][x + 1] = t_nil;
+		til[y][x - 1] = t_nil;
+		til[y + 1][x + 1] = t_nil;
+		til[y + 1][x - 1] = t_nil;
+		til[y - 1][x - 1] = t_nil;
+		til[y - 1][x + 1] = t_nil;
 	}
 }
